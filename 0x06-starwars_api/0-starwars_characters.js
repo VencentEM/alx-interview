2c1eedb79de3cc55bc583f310ab2795a55caffdb
VencentEM <luxestore1997@gmail.com>
2024-11-18 01:37:27 +0000

    0x06. Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,48 +2,37 @@
 
 const request = require('request');
 
-const fetchCharacterNames = async (url) => {
-  try {
-    const res = await new Promise((resolve, reject) => {
-      request(url, (err, res, body) => {
-        if (err) reject(err);
-        resolve(JSON.parse(body));
-      });
+const fetchCharacterNames = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) reject(err);
+      resolve(JSON.parse(body).characters);
     });
-    return res.characters;
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+  });
 };
 
-const fetchCharacterData = async (characterUrl) => {
-  try {
-    const res = await new Promise((resolve, reject) => {
-      request(characterUrl, (err, res, body) => {
-        if (err) reject(err);
-        resolve(JSON.parse(body).name);
-      });
+const fetchCharacterData = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (err, res, body) => {
+      if (err) reject(err);
+      resolve(JSON.parse(body).name);
     });
-    return res;
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+  });
 };
 
 const main = async () => {
-  try {
-    const movieId = process.argv[2];
-    const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-    const characters = await fetchCharacterNames(filmUrl);
+  const movieId = process.argv[2];
+  const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
+  try {
+    const characters = await fetchCharacterNames(filmUrl);
+    
     for (const character of characters) {
       const name = await fetchCharacterData(character);
       console.log(name);
     }
   } catch (err) {
-    console.error('Error fetching data: ', err);
+    console.error('Error:', err);
   }
 };
 
